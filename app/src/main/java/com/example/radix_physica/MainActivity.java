121c6b30e326406409e569e29b283adca1f78461
KarenGhardashyan <karenkrakin@gmail.com>
2024-01-04 23:06:29 +0400

    Initial commit

---
+++ app/src/main/java/com/example/radix_physica/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.radix_physica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button without_account;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button without_account = findViewById(R.id.without_acc);
+
+        without_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, physics_lobby.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
