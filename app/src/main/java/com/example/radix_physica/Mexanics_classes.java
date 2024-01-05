58aa9d2299f8beedb6ecc20ef8bdbd34e8cabdb8
Karen Ghardashyan <karenkrakin@gmail.com>
2024-01-05 17:25:20 +0400

    Initial commit

---
+++ app/src/main/java/com/example/radix_physica/Mexanics_classes.java
@@ -0,0 +1,14 @@
+package com.example.radix_physica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mexanics_classes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mexanics_classes);
+    }
+}
--- app/src/main/java/com/example/radix_physica/physics_lobby.java
+++ app/src/main/java/com/example/radix_physica/physics_lobby.java
@@ -2,15 +2,30 @@ package com.example.radix_physica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class physics_lobby extends AppCompatActivity {
 
-
+    ImageButton mechanicsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pysics_lobby);
+
+        ImageButton mechanicsButton = findViewById(R.id.mexanics);
+
+        mechanicsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(physics_lobby.this, Mexanics_classes.class);
+                startActivity(intent);
+            }
+        });
     }
 }
