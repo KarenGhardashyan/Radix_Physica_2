d86967f06252ef59c3d5faae1da153337d82686c
Karen Ghardashyan <karenkrakin@gmail.com>
2024-05-20 00:54:51 +0400

    .debug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,20 +37,20 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-database:20.3.1")
+    implementation("com.google.firebase:firebase-database:21.0.0")
     implementation("androidx.security:security-identity-credential:1.0.0-alpha03")
-    implementation("com.google.firebase:firebase-auth:22.3.1")
-    implementation("com.google.firebase:firebase-storage:20.3.0")
-    implementation("com.google.firebase:firebase-firestore:24.10.3")
-    implementation("androidx.activity:activity:1.8.0")
+    implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("com.google.firebase:firebase-storage:21.0.0")
+    implementation("com.google.firebase:firebase-firestore:25.0.0")
+    implementation("androidx.activity:activity:1.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.0")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.20")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.google.firebase:firebase-perf")
     implementation("com.sendgrid:sendgrid-java:4.8.2")
     implementation("com.sun.mail:android-mail:1.6.7")
@@ -58,17 +58,17 @@ dependencies {
     implementation ("com.squareup.picasso:picasso:2.71828")
 
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("com.google.android.material:material:1.0.0")
-    implementation("com.google.firebase:firebase-auth:19.0.0")
-    implementation("com.google.firebase:firebase-database:19.1.0")
-    implementation("com.google.firebase:firebase-core:17.2.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("com.google.firebase:firebase-database:21.0.0")
+    implementation("com.google.firebase:firebase-core:21.1.1")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.rengwuxian.materialedittext:library:2.1.4")
     implementation("de.hdodenhof:circleimageview:3.1.0")
 
     implementation("com.github.bumptech.glide:glide:4.12.0")
     implementation("de.hdodenhof:circleimageview:3.1.0")
-    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
 
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 
@@ -78,6 +78,8 @@ dependencies {
 
     implementation("com.opencsv:opencsv:4.6")
 
-    implementation ("com.google.android.material:material:1.5.0")
+    implementation ("com.google.android.material:material:1.12.0")
     implementation ("com.android.support:appcompat-v7:28.0.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+
 }
